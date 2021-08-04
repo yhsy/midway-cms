2283dd9c0e8fb6b64040bf7dd0422dab71467df2
yuhongshuang <juerui@vip.qq.com>
2021-08-04 09:20:01 +0800

    post请求

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,10 +1,31 @@
-import { Controller, Get, Provide } from '@midwayjs/decorator';
+/*
+ * @Author: your name
+ * @Date: 2021-08-03 09:20:07
+ * @LastEditTime: 2021-08-04 09:19:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \midway-cms\src\controller\home.ts
+ */
+import { Controller, Get, Provide, Post } from '@midwayjs/decorator';
 
 @Provide()
 @Controller('/')
 export class HomeController {
+  // Get请求
   @Get('/')
   async home() {
     return 'Hello Midwayjs!';
+  }
+
+  // Post请求
+  @Post('/post')
+  async postData() {
+    // return 'This is a post method'
+    return {
+      code: 1000, // 业务状态码先暂定10000为成功
+      success: true, // true | false, boolean是否成功
+      message:'操作成功',  // 不同的业务提示信息
+      data:{} // 此处是业务数据,类型Object | Array | null
+    }
   }
 }
