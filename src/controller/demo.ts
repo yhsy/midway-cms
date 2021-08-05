4eaf0ba6bb36db1c55f509c914970a73e9696806
yuhongshuang <juerui@vip.qq.com>
2021-08-05 10:03:47 +0800

    获取参数对象

--- src/controller/demo.ts
+++ src/controller/demo.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-08-05 09:00:41
- * @LastEditTime: 2021-08-05 09:23:24
+ * @LastEditTime: 2021-08-05 09:46:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \midway-cms\src\controller\user.ts
  */
-import { Controller,Provide,Get,Query, } from "@midwayjs/decorator";
+import { Controller,Provide,Get,Query, ALL, } from "@midwayjs/decorator";
 
 @Provide()
 @Controller('/api/demo')
@@ -28,6 +28,16 @@ export class DemoController{
             success: true,
             message:'查询成功', 
             data:{uid:Number(uid)} // 返回用户id
+        }
+    }
+
+    @Get('/info')
+    async getDemoQueryInfo(@Query(ALL) queryObject: object){
+        return {
+            code: 1000,
+            success: true,
+            message:'查询成功', 
+            data:{queryObject} // 返回用户信息
         }
     }
 }
